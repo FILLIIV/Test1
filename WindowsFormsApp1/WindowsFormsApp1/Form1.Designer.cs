3fc7f1c43e9b10d37aaa9d377cdbbef44d0ebd7d
GeeksforGeeks <stalker-volk93@mail.ru>
2022-11-04 03:27:07 +0400

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label11 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label1.Location = new System.Drawing.Point(40, 27);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(281, 176);
+			this.label1.TabIndex = 13;
+			this.label1.Text = "Введите стороны фигуры";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(56, 50);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(57, 13);
+			this.label2.TabIndex = 14;
+			this.label2.Text = "1 сторона";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(56, 74);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(57, 13);
+			this.label3.TabIndex = 15;
+			this.label3.Text = "2 сторона";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(56, 100);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(57, 13);
+			this.label4.TabIndex = 16;
+			this.label4.Text = "3 сторона";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(141, 50);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 17;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(141, 74);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 18;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(141, 100);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 19;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(257, 57);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(22, 13);
+			this.label5.TabIndex = 20;
+			this.label5.Text = "См";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(257, 81);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(22, 13);
+			this.label6.TabIndex = 21;
+			this.label6.Text = "См";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(257, 103);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(22, 13);
+			this.label7.TabIndex = 22;
+			this.label7.Text = "См";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(123, 148);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(106, 40);
+			this.button1.TabIndex = 23;
+			this.button1.Text = "Площадь фигуры";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(12, 284);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(35, 13);
+			this.label8.TabIndex = 24;
+			this.label8.Text = "label8";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(179, 277);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 25;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(535, 148);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(106, 40);
+			this.button2.TabIndex = 36;
+			this.button2.Text = "Площадь окружности";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(657, 57);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(22, 13);
+			this.label11.TabIndex = 33;
+			this.label11.Text = "См";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(541, 50);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 30;
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Location = new System.Drawing.Point(456, 50);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(43, 13);
+			this.label14.TabIndex = 27;
+			this.label14.Text = "Радиус";
+			// 
+			// label15
+			// 
+			this.label15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label15.Location = new System.Drawing.Point(440, 27);
+			this.label15.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(281, 176);
+			this.label15.TabIndex = 26;
+			this.label15.Text = "Введите стороны фигуры";
+			this.label15.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(541, 277);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 38;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(418, 280);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(117, 13);
+			this.label9.TabIndex = 37;
+			this.label9.Text = "Площадь окружности";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.label14);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label9;
+	}
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		SqlConnection con;
+		SqlDataAdapter da;
+		SqlCommand cmd;
+		DataSet ds;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public void GetList()
+		{
+			con = new SqlConnection(@"Data Source= .\SQLEXPRESS; Initial Catalog=Base1; Integrated Security=True");
+			da = new SqlDataAdapter("SELECT Таблица1.IdCat, Таблица1.IdProd, *" +
+"FROM Таблица1" +
+"GROUP BY Таблица1.IdCat, Таблица1.IdProd", con) ;
+			ds = new DataSet();
+			con.Open();
+			da.Fill(ds, "Tabel1");
+			//dataGridView1.DataSource = ds.Tables["Tabel1"];//Таблица для заполнения
+			con.Close();
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			double S;
+			if ((textBox1.Text=="" & textBox2.Text=="" & textBox3.Text=="") ^ (textBox1.Text != "" & textBox2.Text == "" & textBox3.Text == ""))
+			{
+				label8.Text = "ВВЕДИТЕ минимум 2 стороны";
+			}
+			else
+			{
+				if (textBox1.Text != "" & textBox2.Text != ""& textBox2.Text == "")
+				{
+					if (textBox1.Text == textBox2.Text)
+					{
+						label8.Text = "Площадь квадрата = ";
+						S = double.Parse(textBox1.Text) * 2;
+						textBox4.Text = S.ToString();
+					}
+					else
+					{
+						label8.Text = "Площадь прямоугольника = ";
+						S = double.Parse(textBox1.Text) * double.Parse(textBox2.Text);
+						textBox4.Text = S.ToString();
+					}
+				}
+				else 
+				{
+					if (double.Parse(textBox3.Text) * double.Parse(textBox3.Text) == double.Parse(textBox1.Text) * double.Parse(textBox1.Text) + double.Parse(textBox2.Text) * double.Parse(textBox2.Text))
+					{
+						label8.Text = "Площадь прямоугольного треугольника = ";
+						S = (double.Parse(textBox1.Text) * double.Parse(textBox2.Text))/2;
+						textBox4.Text = S.ToString();
+					}
+					else
+					{
+						double H, p;
+						p = (double.Parse(textBox1.Text) + double.Parse(textBox2.Text) + double.Parse(textBox3.Text)) / 2;
+						label8.Text = "Площадь треугольника = ";
+						S = Math.Sqrt(p*(p-double.Parse(textBox1.Text))* (p - double.Parse(textBox2.Text))* (p - double.Parse(textBox3.Text)));
+						textBox4.Text = S.ToString();
+					}
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			double S;
+			S = Math.PI * (double.Parse(textBox7.Text) * double.Parse(textBox7.Text));
+			textBox5.Text = S.ToString();
+		}
+	}
+}
